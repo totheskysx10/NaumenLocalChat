9c862b8c7397417762c265e694f2345d5b0b8df8
Totheskysx10 <crazycat87654@gmail.com>
2025-04-22 14:10:18 +0500

    initial

---
+++ src/main/java/ru/naumen/naumenlocalchat/NaumenLocalChatApplication.java
@@ -0,0 +1,13 @@
+package ru.naumen.naumenlocalchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaumenLocalChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaumenLocalChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/naumen/naumenlocalchat/NaumenLocalChatApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.naumen.naumenlocalchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaumenLocalChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

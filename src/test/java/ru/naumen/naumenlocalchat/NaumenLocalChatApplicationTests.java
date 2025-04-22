fd73b7d8d16b88495d332640b7c410103a9ac38b
Totheskysx10 <crazycat87654@gmail.com>
2025-04-22 14:11:25 +0500

    added readme and dependencies

--- src/test/java/ru/naumen/naumenlocalchat/NaumenLocalChatApplicationTests.java
+++ src/test/java/ru/naumen/naumenlocalchat/NaumenLocalChatApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class NaumenLocalChatApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

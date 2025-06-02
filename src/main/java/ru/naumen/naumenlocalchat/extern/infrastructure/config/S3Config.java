cd04c3004c7c87c8b6988f68b2d4ff378ae1b049
Totheskysx10 <crazycat87654@gmail.com>
2025-06-02 16:28:45 +0500

    S3 fix

--- src/main/java/ru/naumen/naumenlocalchat/extern/infrastructure/config/S3Config.java
+++ src/main/java/ru/naumen/naumenlocalchat/extern/infrastructure/config/S3Config.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
